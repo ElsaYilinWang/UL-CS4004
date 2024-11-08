e9789f8a84b4ae88a17bf5f8416a4b2ac91260bd
elsawang <elsa.yilin.w39@gmail.com>
2024-11-08 14:29:31 +0000

    CS4004 Assignment2 starting project

---
+++ src/test/java/BankAccountManagementSystemTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountManagementSystemTest {
+
+    @Test
+    void createAccount() {
+    }
+
+    @Test
+    void deposit() {
+    }
+
+    @Test
+    void withdraw() {
+    }
+
+    @Test
+    void getAccountBalance() {
+    }
+}
