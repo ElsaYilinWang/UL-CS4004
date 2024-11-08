dc4a27bd72eea8f6cdb85bdffdb5c6c6e0b8e0c9
elsawang <elsa.yilin.w39@gmail.com>
2024-11-08 14:56:01 +0000

    CS4004 Assignment2 waiting for test requirements

--- src/test/java/BankAccountManagementSystemTest.java
+++ src/test/java/BankAccountManagementSystemTest.java
@@ -1,11 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankAccountManagementSystemTest {
 
     @Test
+    @DisplayName("Create a new bank account")
     void createAccount() {
+
     }
 
     @Test
